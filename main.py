499787c3b5a44ca7a0dc0a722324fd80074d0628
nastya.kyrgys@mail.ru <nastya.kyrgys@mail.ru>
2023-05-17 21:20:12 +0700

    Second function

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 # This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def generate_number():
+    return random.randint(1, 200)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+
 
